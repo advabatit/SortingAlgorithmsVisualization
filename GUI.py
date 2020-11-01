f142282c08f2cdc557e22c8a55527e6e412e14e5
advabatit <advabatit@gmail.com>
2020-11-01 13:54:48 +0200

    Organized the file. Splited big functions to small so every function will  only have one job

--- GUI.py
+++ GUI.py
@@ -13,22 +13,21 @@ PAD_X, PAD_Y = 5, 5
 root = tkinter.Tk()
 UI_frame = tkinter.Frame(root, width = FRAME_WIDTH, height = FRAME_HEIGHT, bg = 'grey')
 canvas = tkinter.Canvas(root, width = CANVAS_WIDTH, height = CANVAS_HEIGHT, bg = 'white')
+scales_data = [[0, 10, 'Select Size'],
+                [0, 10, 'Select min value'],
+                [11, 100, 'Select max value']]
+                
 data = []
 
 def main():
 
-    root.title("Sorting Algorithms Visualization")
-    root.maxsize(MAX_WIDTH, MAX_HEIGHT)
-    root.config(bg='black')    
-    
-    UI_frame.grid(row = 0, column = 0, padx = PAD_X_LAYOUT, pady = PAD_Y_LAYOUT)
-    canvas.grid(row = 1, column = 0, padx = PAD_X_LAYOUT, pady = PAD_Y_LAYOUT)
-    
     # Variables 
     selected_algorithm = tkinter.StringVar()
     algorithm_list = ['Bubble Sort', 'Merge Sort']
+    buttons_data = [['Start', lambda : start_sort(speed_scale.get())],
+                    ['Generate', lambda : generat(min_value_entery, max_value_entery, size_entery)]]
 
-    # TO DO: Create function to create entry more generic
+    base_layout()
 
     algorithm_label = tkinter.Label(UI_frame, text = 'Algorithm', bg = 'grey')
     algorithm_label.grid(row = 0, column = 0, padx = PAD_X, pady = PAD_Y, sticky = 'W')
@@ -37,26 +36,39 @@ def main():
     algorithm_menu.grid(row = 0, column = 1, padx = PAD_X, pady = PAD_Y, sticky = 'W')
     algorithm_menu.current(0)
 
-    speed_scale = tkinter.Scale(UI_frame, from_ = 0.1, to = 2.0, length = 200, digits = 2, resolution = 0.2, orient = 'horizontal', label = 'Select speed [s]')
-    speed_scale.grid(row = 0, column = 2, padx = PAD_X, pady = PAD_Y)
-
-    srart_button = tkinter.Button(UI_frame, text = 'Start', command = lambda : start_sort(speed_scale.get()))
-    srart_button.grid(row = 0, column = 3, padx = PAD_X, pady = PAD_Y, sticky = 'W')
-
-    size_entery = tkinter.Scale(UI_frame, from_ = 3, to = 20, resolution = 1, orient = 'horizontal', label = 'Select size')
-    size_entery.grid(row = 1, column = 0, padx = PAD_X, pady = PAD_Y)
-
-    min_value_entery = tkinter.Scale(UI_frame, from_ = 0, to = 10, resolution = 1, orient = 'horizontal', label = 'Select min value')
-    min_value_entery.grid(row = 1, column = 1, padx = PAD_X, pady = PAD_Y)
-
-    max_value_entery = tkinter.Scale(UI_frame, from_ = 11, to = 100, resolution = 1, orient = 'horizontal', label = 'Select max value')
-    max_value_entery.grid(row = 1, column = 2, padx = PAD_X, pady = PAD_Y)
-
-    generate_button = tkinter.Button(UI_frame, text = 'Generate', command = lambda : generat(min_value_entery, max_value_entery, size_entery))
-    generate_button.grid(row = 1, column = 3, padx = PAD_X, pady = PAD_Y, sticky = 'W')
+    size_entery, min_value_entery, max_value_entery, speed_scale = init_scale()
+    init_buttons(buttons_data)
 
     root.mainloop()
 
+
+def base_layout():
+    root.title("Sorting Algorithms Visualization")
+    root.maxsize(MAX_WIDTH, MAX_HEIGHT)
+    root.config(bg='black')    
+    
+    UI_frame.grid(row = 0, column = 0, padx = PAD_X_LAYOUT, pady = PAD_Y_LAYOUT)
+    canvas.grid(row = 1, column = 0, padx = PAD_X_LAYOUT, pady = PAD_Y_LAYOUT)
+
+def init_scale():
+    global scales_data
+    values = [] # values[0] = size, values[1] = min, values[2] = max
+
+    for i in range(len(scales_data)):
+        values.append(tkinter.Scale(UI_frame, from_ = scales_data[i][0], to = scales_data[i][1], resolution = 1, orient = 'horizontal', label = scales_data[i][2]))
+        values[i].grid(row = 1, column = i, padx = PAD_X, pady = PAD_Y)
+
+    speed_scale = tkinter.Scale(UI_frame, from_ = 0.1, to = 2.0, length = 200, digits = 2, resolution = 0.2, orient = 'horizontal', label = 'Select speed [s]')
+    speed_scale.grid(row = 0, column = 2, padx = PAD_X, pady = PAD_Y)
+
+    return values[0], values[1], values[2], speed_scale
+
+def init_buttons(buttons_data : list):
+    values = []
+
+    for i in range(len(buttons_data)):
+        values.append(tkinter.Button(UI_frame, text = buttons_data[i][0], command = buttons_data[i][1]))
+        values[i].grid(row = i, column = 3, padx = PAD_X, pady = PAD_Y, sticky = 'W')
 
 def generat(min : tkinter.Entry, max : tkinter.Entry, size_entery : tkinter.Entry):
     global data
@@ -68,6 +80,7 @@ def generat(min : tkinter.Entry, max : tkinter.Entry, size_entery : tkinter.Entr
 def start_sort(time):
     global data
     bubble_sort(data, draw_sort, time)
+
 
 def get_data(min : tkinter.Entry, max : tkinter.Entry, size_entery : tkinter.Entry) -> list:
     global data
